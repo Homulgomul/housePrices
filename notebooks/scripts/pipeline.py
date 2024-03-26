830c6dcdaa33ac71164d5a869b385d8edafe0221
Luke Roth <126115167+Homulgomul@users.noreply.github.com>
2024-03-26 19:55:11 +0000

    Addded pipeline

---
+++ notebooks/scripts/pipeline.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import sklearn.preprocessing as preprocessing
+import sklearn.impute as impute
+
+#define pipeline for preprocessing as function
+def preprocess(data):
+    data = pd.get_dummies(data)
+    data['LotFrontage'] = data['LotFrontage'].fillna(0)
+    imputer = impute.KNNImputer(n_neighbors=5)
+    data = pd.DataFrame(imputer.fit_transform(data), columns=data.columns)
+    scaler = preprocessing.StandardScaler()
+    data = pd.DataFrame(scaler.fit_transform(data), columns=data.columns)
+    return data
