77d3425599c44c4dc3488ee9205c52accdf5edb5
Luke Roth <126115167+Homulgomul@users.noreply.github.com>
2024-03-28 04:11:51 +0000

    added pipeline functionality for vetting outliers, provisionally set to outer 0.2%, fixed Id column

--- notebooks/scripts/pipeline.py
+++ notebooks/scripts/pipeline.py
@@ -5,7 +5,11 @@ import sklearn.impute as impute
 #define pipeline for preprocessing as function
 def Pipeline(data):
     data = pd.get_dummies(data)
+    data = data.drop("Id", axis=1)
     data['LotFrontage'] = data['LotFrontage'].fillna(0)
+    for col in data.columns:
+        if (data[col].dtype == 'int64' or data[col].dtype == 'float64'):
+            data[col] = data[col].clip(lower=data[col].quantile(0.001), upper=data[col].quantile(0.999))
     imputer = impute.KNNImputer(n_neighbors=5)
     data = pd.DataFrame(imputer.fit_transform(data), columns=data.columns)
     scaler = preprocessing.StandardScaler()
